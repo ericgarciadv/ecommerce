36d4a52c978b6aac98b9d9890bfdfefe41e22769
Eric Garcia <ericgarcia.rodrigues@gmail.com>
2018-08-04 20:58:40 -0300

    I created the class Page

---
+++ vendor/grdev/php-classes/src/Page.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Egrdev;
+
+use Rain\tpl;
+
+class Page {
+
+	private $tpl;
+	private $options;
+	private $defaults = array(
+		"data"=>[]
+	);
+
+	public function __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults,$opts);
+
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+	    );
+
+		Tpl::configure($config);
+
+		$this->tpl = new Tpl;
+
+		$this->setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+	}
+
+	private function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key,$value);
+		}		
+	}
+
+	public function setTpl($nome, $data = array(), $returnHTML = false)
+	{
+		$this->setData($data);
+
+		return $this->tpl->draw("$nome",$returnHTML);
+	}
+
+	public function __destruct(){
+		$this->tpl->draw("footer");
+	}
+
+
+}
+
+
+?>
