cd8ffe22617271b242c31a3b8f4a951360c0d796
Eric Garcia <ericgarcia.rodrigues@gmail.com>
2018-08-06 23:04:32 -0300

    git commit -m

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Egrdev\Page;
 use \Egrdev\PageAdmin;
+use \Egrdev\Model\User;
 
 $app = new Slim();
 
@@ -20,10 +21,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->get('/admin/logout', function() {
+    
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"],$_POST["password"]);
+
+	header("Location: /admin");
+	exit;
 });
 
 $app->run();
